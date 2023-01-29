9b127809e7aceb2740645fabd0b564295a034b5d
anazz12312 <anashesham000@gmail.com>
2023-01-30 00:56:42 +0200

    dumb data

---
+++ src/requests/test.js
@@ -0,0 +1,28 @@
+const data = [
+    {
+        name: "anas",
+        id:1,
+        total: 900,
+        sub1: 175,
+        sub2: 180,
+        sub3: 100,
+        sub4: 165,
+        sub5: 180,
+        sub6: 100
+    },
+    {
+        name: "zyad",
+        id:2,
+        total: 950,
+        sub1: 175,
+        sub2: 190,
+        sub3: 110,
+        sub4: 175,
+        sub5: 190,
+        sub6: 110
+    }
+]
+const getData = () => {
+    return data;
+}
+export default getData;
