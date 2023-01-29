2892cd13373f26581b07be6b43e5b5716d29d5c4
Zyad Abdul-Nasser <zizo_007@live.com>
2023-01-30 00:23:55 +0200

    Routes&Comp

---
+++ src/router/AppRouter.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+import Home from '../components/Home';
+import NotFound from '../components/NotFound';
+import Data from '../components/Data';
+
+const AppRouter = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path='*' element={<NotFound />} />
+                <Route path='/' element={<Home />} />
+                <Route path='/data' element={<Data />} />
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default AppRouter;
