d92bf7b41212e8c9c81f819f535b405c23322979
Zyad Abdul-Nasser <zizo_007@live.com>
2023-01-31 02:02:36 +0200

    'z'

--- src/components/LogInModal.js
+++ src/components/LogInModal.js
@@ -1,37 +1,10 @@
 import * as React from 'react';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import Modal from '@mui/material/Modal';
-import ButtonGroup from '@mui/material/ButtonGroup';
-import { FormGroup, Input } from '@mui/material';
-import IconButton from '@mui/material/IconButton';
-import FilledInput from '@mui/material/FilledInput';
-import OutlinedInput from '@mui/material/OutlinedInput';
-import InputLabel from '@mui/material/InputLabel';
-import InputAdornment from '@mui/material/InputAdornment';
-import FormHelperText from '@mui/material/FormHelperText';
-import FormControl from '@mui/material/FormControl';
-import TextField from '@mui/material/TextField';
-import Visibility from '@mui/icons-material/Visibility';
-import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import { useNavigate } from 'react-router-dom';
 
-
-
-const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
-    boxShadow: 24,
-    p: 4,
-    display: 'grid'
-};
 
 export default function LogInModal({ isOpen, setIsOpen }) {
-
+    const nav = useNavigate();
     return (
         <Modal
             open={isOpen}
@@ -39,16 +12,17 @@ export default function LogInModal({ isOpen, setIsOpen }) {
             aria-labelledby="modal-modal-title"
             aria-describedby="modal-modal-description"
         >
-            <Box sx={style} component="form">
-                <h3>Log In</h3>
-                <TextField id="standard-basic" label="Username" variant="outlined" />
-                <TextField
-                    id="outlined-password-input"
-                    label="Password"
-                    type="password"
-                    autoComplete="current-password"
-                />
-            </Box>
+            <form className='formm' onSubmit={(e) => {
+                e.preventDefault();
+                if(e.target.elements.username.value === 'AZ' && e.target.elements.pass.value === 'hacker 3rs')  {
+                    nav('/admin');
+                }
+            }}>
+                <h3 style={{paddingLeft: '75px'}}>Log In</h3>
+                <input  style={{padding:'10px',margin:'10px'}} type={'text'} name='username' placeholder='Username'/>
+                <input  style={{padding:'10px',margin:'10px'}} name='pass' placeholder='Password' type="password" />
+                <button style={{padding:'10px',margin:'10px'}}>Login</button>
+            </form>
         </Modal>
     );
 }
